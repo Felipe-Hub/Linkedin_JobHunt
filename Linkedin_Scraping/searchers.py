21c4a07dde499c267ce27f70c0c45ff0dd4b239e
Felipe Rocha <felipe.chaves.rocha@gmail.com>
2020-06-19 23:51:10 +0100

    Scraped 1000 LinkedIn Data Engineering position "worldwide" (mostly from US).
    Solved issue with selenium not finding the elements containing the job info by using driver.find.element_by_xpath() instead of by_class_name().
    
    Future Features:
    * possibility to choose how many days since the job offer was posted;
    * finish function that takes a list of job titles and locations;

--- Linkedin_Scraping/searchers.py
+++ Linkedin_Scraping/searchers.py
@@ -1,22 +1,23 @@
-from random import uniform
+from selenium.webdriver.common.keys import Keys
 from selenium.webdriver import Firefox
+from random import uniform
 import pandas as pd
-import selenium
 import time
+import csv
 
 
-def single_job_search(job=None, location=None, max_results=100):
+def single_job_search(job='', location='', max_result=100):
     """
-    Choose job title (or company) and location to look for and scrape job offers in Linkedin.
+    Choose job title (or company) and location to look for and scrape job offers in LinkedIn.
     If no parameters is passed it will perform a general/global job search to scrape from.
 
-    :param: job: takes a string to input in Linkedin job search bar.
-    :param: location: takes a string to input in Linkedin location search bar.
+    :param: job: takes a string to input in LinkedIn job search bar.
+    :param: location: takes a string to input in LinkedIn location search bar.
     :param: max_results: maximum number of job offers you want to scrape.
-    :return: pandas.DataFrame with job offer information
+    :return: a tuple with the DataFrame with job offer information and the list of errors (results, errors).
     """
 
-    # starts driver on Linkedin job search page
+    # starts driver on LinkedIn job search page
     url = 'https://www.linkedin.com/jobs'
     driver = Firefox()
     time.sleep(1)
@@ -32,29 +33,145 @@ def single_job_search(job=None, location=None, max_results=100):
     # location input
     loc_bar = driver.find_elements_by_xpath('//input[@aria-controls="job-search-bar-location-typeahead-list"]')
     loc_bar[1].clear()
-    loc_bar[1].send_keys(location)
-    time.sleep(1)
-    loc_bar[1].click()
-    # clicke search button
-    driver.find_elements_by_xpath('//button[@class="search__button pill pill--blue etta-pill"]')[1].click()
-    time.sleep(1)
+    # sometime the dropdown menu obscures the search button, to solve it:
+    # only write location if it is specified
+    if location != '':
+        loc_bar[1].send_keys(location)
+        time.sleep(1)
+        # sometimes the location does not have a drop down menu, so we have to click the search button
+        if len(driver.find_elements_by_id('location-1')) > 0:
+            driver.find_elements_by_id('location-1')[0].click()
+            time.sleep(1)
+        else:
+            driver.find_elements_by_xpath('//button[@class="search__button pill pill--blue etta-pill"]')[1].click()
+            time.sleep(1)
+    # click search button if no location is specified
+    else:
+        driver.find_elements_by_xpath('//button[@class="search__button pill pill--blue etta-pill"]')[1].click()
+        time.sleep(1)
 
-    jobs_info = {'links': links_scraped = list(), }
-    while len(links_scraped) < max_results:
+    elements = list()
+    while len(elements) < int(max_result):
+        breaker = len(elements)
+        # store each job offer card element
         elements = driver.find_elements_by_class_name('result-card__full-card-link')
-        links = [element.get_attribute('href') for element in elements]
-        links_scraped += links
         elements[-1].send_keys(Keys.NULL)
-        time.sleep(uniform(1, 2))
+        time.sleep(uniform(1, 1.5))
+        # this if statement will break the while loop if there are no more job offers available:
+        if len(elements) == breaker:
+            break
+        # in case infinite scroll ends click "show more" button
+        show_more = driver.find_elements_by_xpath('//button[@class="infinite-scroller__show-more-button '
+                                                  'infinite-scroller__show-more-button--visible"]')
+        if len(show_more) > 0:
+            show_more[0].click()
+
+    # create data structures to store scraped info
+    job_title = list()
+    location = list()
+    company = list()
+    applicants = list()
+    days = list()
+    body = list()
+    seniority = list()
+    employment = list()
+    function = list()
+    industry = list()
+
+    # get links for job offers
+    links_scraped = [element.get_attribute('href') for element in elements]
+    # empty list to store possible errors
+    errors = list()
+    for element in elements:
+        # the try and excepts are import:
+        # the first one will check availability of link to job description, if not available appends as an error
+        # the other tries and excepts will check if info is available, it is not available, it will append None
+        # that way we keep the lists with same length for the dict, making it possible to build a pandas.DataFrame
+        try:
+            element.click()
+            time.sleep(uniform(0.5, 1))
+        except:
+            errors.append(element.get_attribute('href'))  # stores links of failed job offer click
+        try:
+            job_title.append(driver.find_element_by_class_name(
+                'topcard__title').text)
+        except:
+            job_title.append(None)
+        try:
+            location.append(driver.find_element_by_xpath(
+                '//span[@class="topcard__flavor topcard__flavor--bullet"]').text)
+        except:
+            location.append(None)
+        try:
+            company.append(driver.find_element_by_xpath(
+                '//a[@class="topcard__org-name-link topcard__flavor--black-link"]').text)
+        except:
+            company.append(None)
+        try:
+            applicants.append(driver.find_element_by_xpath(
+                '//figcaption[@class="num-applicants__caption"]').text)
+        except:
+            applicants.append(None)
+        try:
+            days.append(driver.find_element_by_xpath(
+                '//span[@class="topcard__flavor--metadata posted-time-ago__text"]').text)
+        except:
+            days.append(None)
+        try:
+            body.append(driver.find_element_by_xpath(
+                '//div[@class="show-more-less-html__markup show-more-less-html__markup--clamp-after-5"]').text)
+        except:
+            body.append(None)
+        try:
+            seniority.append(driver.find_elements_by_xpath(
+                '//span[@class="job-criteria__text job-criteria__text--criteria"]')[0].text)
+        except:
+            seniority.append(None)
+        try:
+            employment.append(driver.find_elements_by_xpath(
+                '//span[@class="job-criteria__text job-criteria__text--criteria"]')[1].text)
+        except:
+            employment.append(None)
+        try:
+            function.append(driver.find_elements_by_xpath(
+                '//span[@class="job-criteria__text job-criteria__text--criteria"]')[2].text)
+        except:
+            function.append(None)
+        try:
+            industry.append(driver.find_elements_by_xpath(
+                '//span[@class="job-criteria__text job-criteria__text--criteria"]')[3].text)
+        except:
+            industry.append(None)
+
+        time.sleep(uniform(0.5, 1))
+
+    jobs_info = {'job title': job_title, 'company': company, 'location': location, 'applicants': applicants,
+                 'days ago': days, 'body': body, 'seniority level': seniority, 'employment type': employment,
+                 'function': function, 'industry': industry, 'links': links_scraped}
+
+    df = pd.DataFrame.from_dict(jobs_info)
+    print("You have successfully scraped {} job offers".format(len(df)))
+    print("Saving results to file!")
+    df.to_csv('search_results.csv')
+
+    if len(errors) > 0:
+        print("Failed scraping {} job offers".format(len(errors)))
+        print("Saving errors to file!")
+        with open('search_errors.csv', 'w', newline='') as outfile:
+            writer = csv.writer(outfile)
+            writer.writerow(errors)
+
+    #driver.quit()
+
+    return df, errors
 
 
-    df = pd.DataFrame.from_dict()
-
-    return df
+"""
+TO DO:
 
 
 def job_search(job=None, location=None, max_results=100):
-    """
+    
     Choose job title(s) (or companies) and location(s) to look for and scrape job offers in Linkedin.
     If no parameters is passed it will perform a general/global job search to scrape from.
 
@@ -62,7 +179,7 @@ def job_search(job=None, location=None, max_results=100):
     :param: location: takes a string or list of strings to input in Linkedin location search bar.
     :param: max_results: maximum number of job offer you want to scrape.
     :return: pandas.DataFrame
-    """
+   
 
 
     # verifies type of input
@@ -83,4 +200,4 @@ def job_search(job=None, location=None, max_results=100):
 
     return
 
-test
+"""
